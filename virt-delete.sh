4697d752f3bb95316715f1f440616d7dba3e6ea1
Ellis Rahhal <erahhal@netflix.com>
2024-01-10 19:10:19 -0800

    removed cruft

--- virt-delete.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-virsh destroy --graceful homefree-fedora
-virsh undefine homefree-fedora
-virsh pool-destroy homefree-fedora
-rm -rf ~/.local/share/images/homefree-fedora.qcow2
